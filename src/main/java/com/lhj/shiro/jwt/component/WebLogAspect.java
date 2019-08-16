b538d71a37dda12693aec31f7e8763ac7c89fdb4
LHJ <LHJ8023@outlook.com>
2019-08-17 00:14:54 +0800

    feat: 集成AOP日志，logback日志还没有存储到本地，之后调整，接下来的任务是集成swagger，把日志存到日志文件和数据库

---
+++ src/main/java/com/lhj/shiro/jwt/component/WebLogAspect.java
@@ -0,0 +1,120 @@
+package com.lhj.shiro.jwt.component;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.URLUtil;
+import cn.hutool.json.JSONUtil;
+import com.lhj.shiro.jwt.bo.WebLog;
+import com.lhj.shiro.jwt.utils.HttpContextUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 日志记录切面
+ * @author LHJ
+ */
+@Aspect
+@Component
+@Order(1)
+@Slf4j
+public class WebLogAspect {
+
+    @Pointcut(value = "execution(public * com.lhj.shiro.jwt.controller.*.*(..))")
+    public void weblog(){}
+
+    @Before("weblog()")
+    public void before(JoinPoint joinPoint) throws Throwable{}
+
+    @AfterReturning(value = "weblog()", returning = "ret")
+    public void afterReturning(Object ret) throws Throwable{}
+
+    @Around("weblog()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable{
+        long startTime = System.currentTimeMillis();
+        //获取当前请求对象
+        HttpServletRequest request = HttpContextUtil.getHttpServletRequest();
+        //记录请求信息(通过Logstash传入Elasticsearch)
+        WebLog webLog = new WebLog();
+        Object result = joinPoint.proceed();
+        Signature signature = joinPoint.getSignature();
+        MethodSignature methodSignature = (MethodSignature) signature;
+        Method method = methodSignature.getMethod();
+        //TODO swagger api
+//        if (method.isAnnotationPresent(ApiOperation.class)) {
+//            ApiOperation log = method.getAnnotation(ApiOperation.class);
+//            webLog.setDescription(log.value());
+//        }
+        long endTime = System.currentTimeMillis();
+        String urlStr = request.getRequestURL().toString();
+        webLog.setBasePath(StrUtil.removeSuffix(urlStr, URLUtil.url(urlStr).getPath()));
+        webLog.setIp(request.getRemoteUser());
+        webLog.setMethod(request.getMethod());
+        webLog.setParameter(getParameter(method, joinPoint.getArgs()));
+        webLog.setResult(result);
+        webLog.setSpendTime((int) (endTime - startTime));
+        webLog.setStartTime(startTime);
+        webLog.setUri(request.getRequestURI());
+        webLog.setUrl(request.getRequestURL().toString());
+        //TODO logMap 记录到elasticSearch ... 后期补充
+        Map<String,Object> logMap = new HashMap<>();
+        logMap.put("url",webLog.getUrl());
+        logMap.put("method",webLog.getMethod());
+        logMap.put("parameter",webLog.getParameter());
+        logMap.put("spendTime",webLog.getSpendTime());
+        logMap.put("description",webLog.getDescription());
+//        LOGGER.info("{}", JSONUtil.parse(webLog));
+        //log.info(Markers.appendEntries(logMap), JSONUtil.parse(webLog).toString());
+        log.info(JSONUtil.parse(webLog).toString());
+        return result;
+    }
+
+    /**
+     * 根据方法和传入的参数获取请求参数
+     */
+    private Object getParameter(Method method, Object[] args) {
+        List<Object> argList = new ArrayList<>();
+        Parameter[] parameters = method.getParameters();
+        for (int i = 0; i < parameters.length; i++) {
+            //将RequestBody注解修饰的参数作为请求参数
+            RequestBody requestBody = parameters[i].getAnnotation(RequestBody.class);
+            if (requestBody != null) {
+                argList.add(args[i]);
+            }
+            //将RequestParam注解修饰的参数作为请求参数
+            RequestParam requestParam = parameters[i].getAnnotation(RequestParam.class);
+            if (requestParam != null) {
+                Map<String, Object> map = new HashMap<>();
+                String key = parameters[i].getName();
+                if (!StringUtils.isEmpty(requestParam.value())) {
+                    key = requestParam.value();
+                }
+                map.put(key, args[i]);
+                argList.add(map);
+            }
+        }
+        if (argList.size() == 0) {
+            return null;
+        } else if (argList.size() == 1) {
+            return argList.get(0);
+        } else {
+            return argList;
+        }
+    }
+
+}
